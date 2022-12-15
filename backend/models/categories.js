e84a595cac88a62531729c1ee942d95cfc6c3e4c
Lucas Maciel <03lucasmaciel@gmail.com>
2022-12-15 19:14:13 +0000

    feat (add) : add new models, categories and events

---
+++ backend/models/categories.js
@@ -0,0 +1,20 @@
+import Sequelize from "sequelize";
+import {
+    dbInstance
+} from "../config/db.js";
+
+const CategoryModel = dbInstance.define(
+    "categories" /* table name */ , {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+    }
+);
+
+export { CategoryModel };
---
+++ backend/models/events.js
@@ -0,0 +1,53 @@
+import Sequelize from "sequelize";
+import {
+    dbInstance
+} from "../config/db.js";
+
+const EventModel = dbInstance.define(
+    "events" /* table name */ , {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        id_category: {
+            type: Sequelize.INTEGER,
+            references: {
+                model: "categories",
+                key: "id",
+            },
+        },
+        name: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+        description: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+        dateStart: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        },
+        dateEnd: {
+            type: Sequelize.DATEONLY,
+            allowNull: false,
+        },
+        value: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+        location: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+        image: {
+            type: Sequelize.STRING(50),
+            allowNull: false,
+        },
+    }
+);
+
+export { EventModel };
+
+
