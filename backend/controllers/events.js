49eeacf688f82a20d5e966314a0b6335f4bf6879
Lucas Maciel <03lucasmaciel@gmail.com>
2022-12-16 20:51:51 +0000

    feat (fix) : controller get all events and replace id_category by category name

--- backend/controllers/events.js
+++ backend/controllers/events.js
@@ -1,28 +1,31 @@
-import { EventModel } from '../models/events.js'
-import { CategoryModel } from '../models/categories.js'
+import {
+  EventModel
+} from '../models/events.js'
+import {
+  CategoryModel
+} from '../models/categories.js'
 
 // get all events and replace id_category by category name
 export const getAllEvents = async (req, res) => {
   const events = await EventModel.findAll({
-    attributes: {
-      exclude: ['id_category']
-    }
   });
+
+  console.log(events)
 
   const events2 = await Promise.all(events.map(async (event) => {
     if (event.id_category !== undefined && event.id_category !== null) {
-      // verifica se o id_category é válido
+      // verify if id_category is not null
       const category = await CategoryModel.findOne({
         where: {
-          id: event.id_category
+          id_category: event.id_category
         },
         attributes: {
-          exclude: ['id']
+          exclude: ['id_category']
         }
       });
 
       if (category !== null) {
-        // verifica se a categoria existe
+        // verify if category exists
         return {
           ...event.dataValues,
           category: category.name
@@ -37,47 +40,56 @@ export const getAllEvents = async (req, res) => {
 }
 
 
-// create new event, verify if dateEnd is greater than dateStart, verify if category exists, verify if value is greater or equal than 0
+// create new event
 export const createEvent = async (req, res) => {
-    const { id_category, name, description, dateStart, dateEnd, value, location, image } = req.body
+  const {
+    id_category,
+    name,
+    description,
+    dateStart,
+    dateEnd,
+    value,
+    location,
+    image
+  } = req.body
 
-    if (dateEnd < dateStart) {
-        return res.status(400).json({
-            message: 'DateEnd must be greater than DateStart'
-        })
-    }
-
-    const category = await CategoryModel.findOne({
-        where: {
-            id: id_category
-        }
+  if (dateEnd < dateStart) {
+    return res.status(400).json({
+      message: 'DateEnd must be greater than DateStart'
     })
+  }
 
-    if (!category) {
-        return res.status(404).json({
-            message: 'Category not found'
-        })
+  const category = await CategoryModel.findOne({
+    where: {
+      id_category: id_category
     }
+  })
 
-    if (value < 0) {
-        return res.status(400).json({
-            message: 'Value must be greater or equal than 0'
-        })
-    }
 
-    await EventModel.create({
-        id_category: id_category,
-        name: name,
-        description: description,
-        dateStart: dateStart,
-        dateEnd: dateEnd,
-        value: value,
-        location: location,
-        image: image
+  if (!category) {
+    return res.status(404).json({
+      message: 'Category not found'
     })
+  }
 
-    return res.status(201).json({
-        message: 'Event created'
+  if (value < 0) {
+    return res.status(400).json({
+      message: 'Value must be greater or equal than 0'
     })
+  }
+
+  await EventModel.create({
+    id_category: id_category,
+    name: name,
+    description: description,
+    dateStart: dateStart,
+    dateEnd: dateEnd,
+    value: value,
+    location: location,
+    image: image
+  })
+
+  return res.status(201).json({
+    message: 'Event created'
+  })
 }
-
