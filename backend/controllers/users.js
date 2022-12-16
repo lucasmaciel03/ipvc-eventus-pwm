6f8e0f42ff4640797e30c70a37183ce06ebd5fbc
Lucas Maciel <03lucasmaciel@gmail.com>
2022-12-16 20:50:26 +0000

    feat (fix) : controller get user by username

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -26,6 +26,16 @@ export const getUserByUsername = async (req, res) => {
             exclude: ["password"],
         },
     });
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found",
+        });
+    }
+
+    return res.status(200).json({
+        user,
+    });
 };
 
 // update username
